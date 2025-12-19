0a8b5508f12219d362c9b2da6d6cf59e29dda09d
chanudevelop <chanu.develop@gmail.com>
2025-12-19 11:00:29 +0900

    스크립트 작성

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-cd /home/ec2-user/backend
 
-JAR_FILE=$(ls build/libs/*-SNAPSHOT.jar | head -n 1)
+# 배포될 경로 (나중에 EC2의 해당 경로를 사용)
+PROJECT_ROOT="/home/ec2-user/backend"
+JAR_FILE=$(ls $PROJECT_ROOT/build/libs/*.jar | head -n 1)
 
-echo "Starting Spring Boot application using $JAR_FILE"
+echo "애플리케이션 실행: $JAR_FILE"
 
-nohup java -jar "$JAR_FILE" > app.log 2>&1 &
+# 로그 파일 위치 지정
+LOG_FILE="$PROJECT_ROOT/application.log"
+
+# 기존 로그 백업 없이 덮어쓰거나 이어쓰기 (여기선 이어쓰기 >> 사용)
+nohup java -jar "$JAR_FILE" >> $LOG_FILE 2>&1 &
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-PID=$(pgrep -f "spring-0.0.1-SNAPSHOT.jar")
+# 현재 구동 중인 애플리케이션 pid 확인
+CURRENT_PID=$(pgrep -f .jar)
 
-if [ -n "$PID" ]; then
-  echo "Stopping existing Spring Boot application (PID: $PID)"
-  kill $PID
+if [ -z $CURRENT_PID ]; then
+  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "현재 구동 중인 애플리케이션 종료 (PID: $CURRENT_PID)"
+  kill -15 $CURRENT_PID
   sleep 5
 fi
