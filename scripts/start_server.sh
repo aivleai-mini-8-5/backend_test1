ad9d83f20bc4391c7f64746442d104d413bc7cc0
chanudevelop <chanu.develop@gmail.com>
2025-12-19 11:46:21 +0900

    start_sh edit

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,7 +2,8 @@
 
 # 배포될 경로 (나중에 EC2의 해당 경로를 사용)
 PROJECT_ROOT="/home/ec2-user/backend"
-JAR_FILE=$(ls $PROJECT_ROOT/build/libs/*.jar | head -n 1)
+# 'plain'이 포함된 파일은 제외
+JAR_FILE=$(ls $PROJECT_ROOT/build/libs/*.jar | grep -v "plain" | head -n 1)
 
 echo "애플리케이션 실행: $JAR_FILE"
 
